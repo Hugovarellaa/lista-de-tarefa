ce701744c8e6117025b1b35f6fd0dcdd5a0aaa3b
Hugo <hugovarellaa@gmail.com>
2022-01-28 22:16:49 -0300

    Criando pasta src

--- myskills/App.js
+++ myskills/App.js
@@ -1,18 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Home } from "./src/pages/Home";
 
 export default function App() {
-  return (
-    <>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>React native</Text>
-      </View>
-    </>
-  );
+  return <Home />;
 }
